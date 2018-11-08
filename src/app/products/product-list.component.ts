dca8ce6d3c6308aedf62ebec4512a21c2efc38f6
memory1 <mshen_85@hotmail.com>
2018-11-08 18:32:12 +0800

    b

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,72 +1,55 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+
 @Component({
-    selector: 'pm-products',
-    templateUrl:'./product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  errorMessage: string;
 
-export class ProductListComponent implements OnInit{
-    private _productService;
-    constructor(productService: ProductService){
-        this._productService = productService;
-    }
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
 
-    onRatingClicked(message:string):void{
-        this.pageTitle = 'Product list:' + message;
-        }
+  filteredProducts: IProduct[];
+  products: IProduct[] = [];
 
-    performFilter(filterBy: string): IProduct[] {
-        filterBy = filterBy.toLowerCase();
-        return this.products.filter((product: IProduct) => product.productName.toLowerCase().indexOf(filterBy) !== -1);
-    }
-    ngOnInit(): void {
-        this.products = this._productService.getProducts();
+  constructor(private productService: ProductService) {
+
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products;
         this.filteredProducts = this.products;
-    }
-    pageTitle:string = "Product List";
-    imageMargin: number = 2;
-    showImage:boolean = false;
-    
-    filteredProducts: IProduct[];
-
-    private _listFilter:string;
-    public get listFilter() {
-        return this._listFilter;
-    }
-    public set listFilter(value) {
-        this._listFilter = value;
-        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter): this.products;
-
-    }
-    products:IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-          }
-    ];
-    imageWidth=40;
-
-    toggleImage():void{
-        this.showImage = !this.showImage;
-    }
+      },
+      error => this.errorMessage = <any>error
+    );
+  }
 }
-
-
--- src/app/shared/convert-to-spaces.pipe.ts
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -1,10 +1,11 @@
-import { Pipe, PipeTransform } from "@angular/core";
-@Pipe({
-     name:'convertToSpaces'
-})
+import { Pipe, PipeTransform } from '@angular/core';
 
+@Pipe({
+  name: 'convertToSpaces'
+})
 export class ConvertToSpacesPipe implements PipeTransform {
-    transform(value: string, character:string): string {
-        return value.replace(character," ");
-    }
+
+  transform(value: string, character: string): string {
+    return value.replace(character, ' ');
+  }
 }
